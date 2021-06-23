523b9f487d788cd3bc132805d1465845c9e06434
rrogers422 <Reagan422440@gmail.com>
2021-06-23 18:42:20 -0400

    added files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/manifest.json',
+    '/indexedDB.js'
+];
