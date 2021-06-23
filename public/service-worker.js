e15cf8ebda1c48deed1af5419091e61e85a88d54
rrogers422 <Reagan422440@gmail.com>
2021-06-23 18:58:13 -0400

    completed service worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,3 +7,53 @@ const FILES_TO_CACHE = [
     '/manifest.json',
     '/indexedDB.js'
 ];
+
+//installing caches
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(files_to_caches))
+        .then(self.skipWaiting())
+    )
+});
+
+// "activate handler" clearing up old caches
+self.addEventListener('activate', (event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.mao((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())        
+    );
+}));
+
+// "fetch handler" 
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse
+                }
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
